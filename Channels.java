e83805fc2563832fb7d8426af40edfa32cb7e852
Q <zqiu1994@gmail.com>
2011-07-18 21:06:47 -0400

    wrote the interface for the channels/ports

--- Channels.java
+++ Channels.java
@@ -1,1 +1,19 @@
-/*config file for all of the ports*/
+
+public interface Channels {
+    //PWM out
+    final int left = 1;
+    final int right = 2;
+    final int arm = 3;
+    final int wrist = 4;
+    final int grip = 5;
+    //InPorts
+    final int arm_angle = 1;
+    final int arm_speed = 2;
+    final int wrist_angle = 3;
+    final int wrist_speed = 4;
+    final int left_angle = 5;
+    final int left_speed = 6;
+    final int right_angle = 7;
+    final int right_speed = 8;
+    final int heading_rate = 9;
+}
