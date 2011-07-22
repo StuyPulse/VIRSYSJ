8b853614b8e9d9398ee6cc1a1d048036661c1a36
Q <zqiu1994@gmail.com>
2011-07-22 16:11:48 -0400

    wrote the robotbase and simple robot subclass. Added the class structure for Client.java

--- VirsysJ/src/Client.java
+++ VirsysJ/src/Client.java
@@ -1,2 +1,7 @@
 /*file that sends input/output info to virsys in a specific order
 refer to /doc/data_format.txt*/
+import java.io.*;
+import java.net.*;
+public class Client implements TorqueConfig,Networkconf{
+
+}
--- VirsysJ/src/FakeWpi.java
+++ VirsysJ/src/FakeWpi.java
@@ -1,1 +1,46 @@
 /* this is imported if the code is not on the real robot*/
+public class FakeWpi implements Channels{
+    public class RobotBase{
+        public RobotBase(){
+
+        }
+        public void IsSystemActive(){
+
+        }
+        public void GetWatchdog(){
+
+        }
+        public void IsEnabled(){
+
+        }
+        public void IsDisabled(){
+
+        }
+        public void IsAutonomous(){
+
+        }
+        public void IsOperatorControlled(){
+            
+        }
+        public void IsNewDataAvailable(){
+
+        }
+    }
+    public class SimpleRobot extends RobotBase{
+        SimpleRobot(){
+            System.out.print("Default Constructor... Overload me!");
+        }
+        public void Disabled(){
+            System.out.print("Default Disabled method... Overload me!");
+        }
+        public void Autonomous(){
+            System.out.print("Default Autonomous method... Overload me!");
+        }
+        public void OperatorControl(){
+            System.out.print("Default OperatorControl method... Overload me!");
+        }
+        public void StartCompetition(){
+            
+        }
+    }
+}
