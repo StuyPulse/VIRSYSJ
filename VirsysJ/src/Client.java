522851ed643dddc258dd3c0947e01a96d108f508
Q <zqiu1994@gmail.com>
2011-07-27 21:53:17 -0400

    wrote the thread structure for client.java

--- VirsysJ/src/Client.java
+++ VirsysJ/src/Client.java
@@ -1,7 +1,53 @@
 /*file that sends input/output info to virsys in a specific order
 refer to /doc/data_format.txt*/
+
 import java.io.*;
 import java.net.*;
-public class Client implements TorqueConfig,Networkconf{
 
+public class Client implements TorqueConfig, Networkconf {
+
+    Runnable sendThread, recieveThread;
+    Thread threadS, threadR;
+
+    public Client() {
+        sendThread = new Send();
+        recieveThread = new Recieve();
+        threadS = new Thread(sendThread);
+        threadR = new Thread(recieveThread);
+    }
+
+    public void sendnrecieve() {
+        threadS.start();
+        threadR.start();
+    }
+
+    public class Send implements Runnable {
+
+        boolean done;
+
+        public void run() {
+            done = false;
+            while (true) {
+                //what the thread will do when it's still running
+                if (done) {
+                    return;
+                }
+            }
+        }
+    }
+
+    public class Recieve implements Runnable {
+
+        boolean done;
+
+        public void run() {
+            done = false;
+            while (true) {
+                //what the thread will do when it's still running
+                if (done) {
+                    return;
+                }
+            }
+        }
+    }
 }
