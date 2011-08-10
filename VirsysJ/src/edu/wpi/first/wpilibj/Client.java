bacdc6b523c0ebf2141df359c591ef0867906c0c
Q <zqiu1994@gmail.com>
2011-08-10 00:17:34 -0400

    wrote the socket used to send and recieve the data. only need to write what to send n recieve

--- VirsysJ/src/edu/wpi/first/wpilibj/Client.java
+++ VirsysJ/src/edu/wpi/first/wpilibj/Client.java
@@ -1,7 +1,7 @@
 /*file that sends input/output info to virsys in a specific order
 refer to /doc/data_format.txt*/
-
 package edu.wpi.first.wpilibj;
+
 import java.io.*;
 import java.net.*;
 
@@ -9,12 +9,20 @@ public class Client implements Networkconf {
 
     private Runnable sendThread, recieveThread;
     private Thread threadS, threadR;
+    private Socket socket;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
 
     public Client() {
         sendThread = new Send();
         recieveThread = new Recieve();
         threadS = new Thread(sendThread);
         threadR = new Thread(recieveThread);
+        try {
+            socket = new Socket(VIRSYS_IP, VIRSYS_RECV_PORT);
+        } catch (IOException ex) {
+            System.out.println(" IOEXCEPTION" + ex.getMessage());
+        }
     }
 
     public void sendnrecieve() {
@@ -28,25 +36,30 @@ public class Client implements Networkconf {
 
         public void run() {
             done = false;
-            while (true) {
+            try {
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            } catch (IOException ex) {
+                System.out.println(" IOEXCEPTION" + ex.getMessage());
+            }
+            while (!done) {
                 //what the thread will do when it's still running
-                if (done) {
-                    return;
-                }
             }
         }
     }
+
     public class Recieve implements Runnable {
 
         boolean done;
 
         public void run() {
             done = false;
-            while (true) {
+            try {
+                dataInputStream = new DataInputStream(socket.getInputStream());
+            } catch (IOException ex) {
+                System.out.println(" IOEXCEPTION" + ex.getMessage());
+            }
+            while (!done) {
                 //what the thread will do when it's still running
-                if (done) {
-                    return;
-                }
             }
         }
     }
