0484a03530c8ddd06e6aa4bae778ccc9ad7029cf
Q <zqiu1994@gmail.com>
2011-08-23 13:33:53 -0400

    worked on encoder. Needs to implement threads so that it can continuously get distance

--- VirsysJ/src/edu/wpi/first/wpilibj/Encoder.java
+++ VirsysJ/src/edu/wpi/first/wpilibj/Encoder.java
@@ -1,26 +1,55 @@
 package edu.wpi.first.wpilibj;
 
-public class Encoder {
+public class Encoder implements Channels{
 
     private Client _c;
     private boolean reverse;
+    private int encodernumber;
+    private double distance;
+    final int wheelradius = 3;
 
     public Encoder(final int aChannel, final int bChannel, boolean reverseDirection, final CounterBase encodingType,Client c) {
         _c = c;
         reverse = reverseDirection;
         if (encodingType == null)
             throw new NullPointerException("Given encoding type was null");
+        switch(aChannel){
+            case (CHANNEL_FRONT_LEFT_ENC_A):
+            case (CHANNEL_FRONT_LEFT_ENC_B):
+                encodernumber = 1;
+                break;
+            case (CHANNEL_REAR_LEFT_ENC_A):
+            case (CHANNEL_REAR_LEFT_ENC_B):
+                encodernumber = 2;
+                break;
+            case (CHANNEL_FRONT_RIGHT_ENC_A):
+            case (CHANNEL_FRONT_RIGHT_ENC_B):
+                encodernumber = 3;
+                break;
+            case (CHANNEL_REAR_RIGHT_ENC_A):
+            case (CHANNEL_REAR_RIGHT_ENC_B):
+                encodernumber = 4;
+                break;
+            default:
+                encodernumber = 0;
+        }
     }
 
+    //returns rate in inches/sec
     public double getRate() {
-        return 0;
+        return encodernumber <= 2 ? _c.getdata()[2] * wheelradius : _c.getdata()[3] * wheelradius;
     }
 
+    //returns distance in inches
     public double getDistance() {
-        return 0;
+       return distance * wheelradius;
     }
     
     public void reset() {
-      //Should do something here?
+        distance = 0;
+    }
+
+    public void start(){
+        reset();
     }
 }
