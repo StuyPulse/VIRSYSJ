5d1112ca2e31d69d685bb10b2855f70e5904d263
Q <zqiu1994@gmail.com>
2011-09-02 10:20:46 -0400

    Wrote the Jaguar class which was copied from the Victor class as they are about the same

--- VirsysJ/src/edu/wpi/first/wpilibj/Jaguar.java
+++ VirsysJ/src/edu/wpi/first/wpilibj/Jaguar.java
@@ -1,20 +1,49 @@
 package edu.wpi.first.wpilibj;
 
-public class Jaguar {
+public class Jaguar implements Channels, TorqueConfig{
+
+    double prevspeed;
+    int channel;
+
+    Client c = CRIO.client;
 
     public Jaguar(int slot, int channel) {
+        this(channel);
     }
 
-    public Jaguar(int channel) {
+    public Jaguar(int _channel) {
+        channel = _channel;
     }
 
     public double get() {
-        return 0;
+        return prevspeed;
     }
 
     public void pidWrite(double output) {
+        prevspeed = output;
+        switch (channel) {
+            case (LEFT_PWM):
+                c.threadS.toSend[0] = (float)(output * LEFT_TORQUE);
+                break;
+            case (RIGHT_PWM):
+                c.threadS.toSend[1] = (float)(output * RIGHT_TORQUE);
+                break;
+            case (ARM_PWM):
+                c.threadS.toSend[2] = (float)(output * ARM_TORQUE);
+                break;
+            case (WRIST_PWM):
+                c.threadS.toSend[3] = (float)(output * WRIST_TORQUE);
+                break;
+            case (GRIP_PWM):
+                c.threadS.toSend[4] = (float)(output * GRIP_TORQUE);
+                break;
+            default:
+                System.out.println("channel does not match any PWM values... try again");
+                break;
+        }
     }
 
     public void set(double speed) {
+        pidWrite(speed);
     }
 }
