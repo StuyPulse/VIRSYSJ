96181dd2f62dd5471c6e0e0956e2b5ee97a998a9
Blake Elias <blakeelias@gmail.com>
2012-01-04 23:43:54 -0500

    Add test for cRIO digital output, run with JUnit in NetBeans

---
+++ VirsysJ/test/edu/wpi/first/wpilibj/CRIOTest.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author blake
+ */
+public class CRIOTest {
+
+    public CRIOTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class CRIO.
+     *
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] arg = null;
+        CRIO.main(arg);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    */
+
+    @Test
+    public void testWriteDigital() {
+        CRIO.DigitalSidecar.register[0] = true;
+        assertEquals(CRIO.DigitalSidecar.register[0], true);
+    }
+}
