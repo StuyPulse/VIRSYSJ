0fba0210eb0b1d012e072733f040cd5ae17fffdc
Zane <sterling.zane@gmail.com>
2012-01-11 18:40:18 -0500

    Added PWM.java and PWMTest.java. Began porting PWM.java.

---
+++ VirsysJ/test/edu/wpi/first/wpilibj/PWMTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 694
+ */
+public class PWMTest {
+
+    public PWMTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void PWM(int channel) {
+    }
+
+    public void PWM(int moduleNumber, int channel) {
+    }
+
+
+}
