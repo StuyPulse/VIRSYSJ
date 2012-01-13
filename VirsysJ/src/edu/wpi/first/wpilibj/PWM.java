31e1b53ed6e5e72e6e35c853ef4383a87ba590c9
Mark <born2run5000@yahoo.com>
2012-01-13 17:05:11 -0500

    Added all methods needed for Servo.java to PWM.java. Also Servo.java now extends PWM.java.

--- VirsysJ/src/edu/wpi/first/wpilibj/PWM.java
+++ VirsysJ/src/edu/wpi/first/wpilibj/PWM.java
@@ -10,5 +10,29 @@ package edu.wpi.first.wpilibj;
  * @author 694
  */
 public class PWM {
+    double position;
+    int raw;
 
+    public PWM(final int channel) {
+    }
+
+    public PWM(final int slot, final int channel) {
+    }
+
+    public void setPosition(double newPosition) {
+        position = newPosition;
+    }
+
+    public double getPosition() {
+        return position;
+    }
+
+    public void setRawTest(int newRaw) {
+        raw = newRaw;
+
+    }
+
+    public int getRawTest() {
+        return raw;
+    }
 }
