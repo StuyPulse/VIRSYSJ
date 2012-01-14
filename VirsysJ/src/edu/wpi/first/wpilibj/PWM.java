a121a8f6c97cdc604c827bebe5d4417562e8a8fd
Mark <born2run5000@yahoo.com>
2012-01-13 19:04:39 -0500

    Added several methods to PWM.java.

--- VirsysJ/src/edu/wpi/first/wpilibj/PWM.java
+++ VirsysJ/src/edu/wpi/first/wpilibj/PWM.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.wpi.first.wpilibj;
 
 /**
@@ -10,29 +9,77 @@ package edu.wpi.first.wpilibj;
  * @author 694
  */
 public class PWM {
-    double position;
-    int raw;
 
-    public PWM(final int channel) {
+    double positionPwm;
+    int rawPwm;
+    int channelPwm;
+    int moduleNumberPwm;
+    int maxPwm;
+    int deadbandMaxPwm;
+    int centerPwm;
+    int deadbandMinPwm;
+    int minPwm;
+    double speedPwm;
+
+    public static class PeriodMultiplier {
+        public final int value;
     }
 
-    public PWM(final int slot, final int channel) {
+    public PWM(int moduleNumber, int channel) {
+        channelPwm = channel;
+        moduleNumberPwm = moduleNumber;
     }
 
-    public void setPosition(double newPosition) {
-        position = newPosition;
+    public PWM(int channel) {
+        channelPwm = channel;
+        //moduleNumber should set to default. What is the default?
+    }
+
+    public void enableDeadbandElimination(boolean eliminateDeadband) {
+        //If eliminateDeadband is true, set the motor curve on the Jaguar to eliminate the deadband in the middle
+        //of the range. Otherwise, keep the full range without modifying any values.
+    }
+
+    public void free() {
+        channelPwm = 0;
+    }
+
+    public void setBounds(int max, int deadbandMax, int center, int deadbandMin, int min) {
+        maxPwm = max;
+        deadbandMaxPwm = deadbandMax;
+        centerPwm = center;
+        deadbandMinPwm = deadbandMin;
+        minPwm = min;
+    }
+    
+    public void setPeriodMultiplier(PWM.PeriodMultiplier mult) {
+
+    }
+    public void setPosition(double position) {
+        positionPwm = position;
+    }
+
+    public void setRaw(int raw) {
+        rawPwm = raw;
+    }
+
+    public int getChannel() {
+        return this.channelPwm;
+    }
+
+    public int getModuleNumber() {
+        return this.moduleNumberPwm;
     }
 
     public double getPosition() {
-        return position;
+        return positionPwm;
     }
 
-    public void setRawTest(int newRaw) {
-        raw = newRaw;
-
+    public int getRaw() {
+        return rawPwm;
     }
 
-    public int getRawTest() {
-        return raw;
+    public double getSpeed() {
+        return speedPwm;
     }
 }
