e87be5bf53b8f1d60f4bc28aaacfc3933582b6e5
Blake Elias <blakeelias@gmail.com>
2012-01-31 00:46:54 -0500

    Move physics and VIRSYS-startup logic to CRIO class

--- VirsysJ/src/crio/hardware/CRIO.java
+++ VirsysJ/src/crio/hardware/CRIO.java
@@ -5,7 +5,7 @@
 
 package crio.hardware;
 import edu.wpi.first.wpilibj.*;
-import utilities.Channels;
+import java.io.*;
 
 /**
  *
@@ -16,22 +16,36 @@ public class CRIO {
     public static int[] virsysOutputMap;
     public static int[] virsysInputMap;
     public static boolean runWithPhysics = false;
+    static Process proc;
 
-    public static void init(int[] virsysOutputMap, int[] virsysInputMap) {
-        runWithPhysics = true;
-        CRIO.client = new Client();
-        CRIO.virsysOutputMap = virsysOutputMap;
-        CRIO.virsysInputMap = virsysInputMap;
+    public static void init(boolean runWithPhysics, int[] virsysOutputMap, int[] virsysInputMap) throws IOException {
+        if (runWithPhysics) {
+            CRIO.runWithPhysics = runWithPhysics;
+            File f = new File("../VIRSYSJ/VIRSYS_alpha");
+            final String osName = System.getProperty("os.name").toLowerCase();
+            if (osName.contains("linux")) {
+                proc = Runtime.getRuntime().exec("./virsys", null, f);
+            }
+            else if (osName.contains("windows")) {
+                proc = Runtime.getRuntime().exec("virsys.exe", null, f);
+            }
+            CRIO.client = new Client();
+            CRIO.virsysOutputMap = virsysOutputMap;
+            CRIO.virsysInputMap = virsysInputMap;
+        }
     }
 
     public static void end() {
         if (CRIO.client != null) {
             CRIO.client.end();
         }
+        if (proc != null) {
+            proc.destroy();
+        }
     }
     
-    public static void run(RobotBase bot, int[] virsysOutputMap, int[] virsysInputMap){
-        CRIO.init(virsysOutputMap, virsysInputMap);
+    public static void run(RobotBase bot, int[] virsysOutputMap, int[] virsysInputMap) throws IOException {
+        CRIO.init(true, virsysOutputMap, virsysInputMap);
         bot.startCompetition();
 	client.end();
     }
